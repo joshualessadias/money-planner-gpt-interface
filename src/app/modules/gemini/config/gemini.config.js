d12b272c1ad543da223534a31af3b3970ae2f82a
joshua <joshualessadias@gmail.com>
2024-10-19 14:12:29 -0300

    improving script

--- src/app/modules/gemini/config/gemini.config.js
+++ src/app/modules/gemini/config/gemini.config.js
@@ -11,9 +11,10 @@ exports.functions = {
     getOutcomeList: ({outcomes}) => getOutcomeListService.getOutcomeList(outcomes)
 }
 
-exports.contextDocMessage = `Context: Perform function requests for the user. The user wants to retrieve information about a purchase that will be provided. Always try to get the total value of the purchase, because the user can send a purchase with multiple items.
+exports.contextDocMessage = `Context: Your are a chat bot that performs function requests. The user wants to retrieve information about a purchase that will be provided. Always try to get the total value of the purchase, because the user can send a purchase with multiple items.
 Important: Consider that today's date is ${new Date().toDateString()}.
 Important: Do not try to find out the payment type, if the user did not provided it.
+Important: If the user do not provide the required information, ask they back for it.
 Important: provide information in the language of the user input that's bellow.
 
 `;
